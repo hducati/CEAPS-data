f38fb6d7bfaf496608fc63a3bf8446506f2242a1
Henrique Miranda <henrique.miranda@CEPEA.ORG.BR>
2019-02-12 14:36:28 -0200

    add groupby function

--- CEAPS/spiders/fetch_values.py
+++ CEAPS/spiders/fetch_values.py
@@ -11,7 +11,8 @@ class FetchValuesSpider(scrapy.Spider):
     start_urls = ['https://www12.senado.leg.br/transparencia/dados-abertos-transparencia/dados-abertos-ceaps']
 
     def parse(self, response):
-        """Primeiramente, vai verificar se existe a pasta chamada
+        """
+        Primeiramente, vai verificar se existe a pasta chamada
         'CEAPS - Data', caso não exista, ela será imediatamente criada, e todo o contéudo
         baixado será salvado dentro dela.
         """
@@ -36,7 +37,8 @@ class FetchValuesSpider(scrapy.Spider):
         self.treating_csv_files()
 
     def make_request(self, filename):
-        """Irá fazer uma requisição de acordo com a url que foi recebida,
+        """
+        Irá fazer uma requisição de acordo com a url que foi recebida,
         transformando em uma lista com o ."split('-')" e pegando
         o último valor com o "[-1]"
 
@@ -48,21 +50,24 @@ class FetchValuesSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.save_csv_file)
 
     def save_csv_file(self, response):
+        # save csv file
         name = response.url.split('/')[-1]
         path = join(getcwd(), "CEAPS - Data\\", name)
         self.logger.info('Saving CSV as %s...' % name)
         with open(path, 'wb') as f:
             f.write(response.body)
             self.logger.info('Successful')
-            print(path)
 
     def treating_csv_files(self):
+        """
+            Todos os arquivos csv serão lidos e agrupados, respectivamente,
+            pelo nome do senador e o mês, e por fim, será calculado o gasto
+            de cada senador por mês.
+        """
         path = join(getcwd(), 'CEAPS - Data\\')
-        directory = listdir(path)
-        for csv_file in directory:
+        for csv_file in listdir(path):
             file_path = join(path, csv_file)
             self.logger.info('Reading %s file...' % file_path.split("\\")[-1])
             df = pd.read_csv(file_path, header=1, encoding="ISO-8859-1", sep=";")
-            df2 = df.set_index("SENADOR", drop=False)
-            value = df2["VALOR_REEMBOLSADO"]
-            # print(df2.groupby(['SENADOR'])['VALOR_REEMBOLSADO'].agg('sum'))
+            df['VALOR_REEMBOLSADO'] = df['VALOR_REEMBOLSADO'].apply(lambda x: float(str(x).split()[0].replace(',', '.')))
+            print(df.groupby(['SENADOR', 'MES'])['VALOR_REEMBOLSADO'].agg('sum'))
